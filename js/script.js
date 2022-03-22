e7d832a769bef5ab0909748093c40834ede333ee
Jennifer Münch <98417960+jcmunch@users.noreply.github.com>
2022-03-22 09:13:55 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var footer = document.querySelector(".contact");
+footer.style.backgroundColor = "#d7ddf3";
+//console.log(footer);
+
+var tagline = document.querySelector(".tagline");
+tagline.innerText = "Adding colors to your life.";
+//console.log(tagline);
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+//console.log(address);
+
+var everyDay = document.querySelector("h3");
+everyDay.innerHTML = "We're here for you <em>every day</em> of the week.";
+//console.log(everyDay);
